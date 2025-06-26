8d390846c66c0e6c7a9f597ee7036b532b544baa
Md Kasif Uddin <mdkasifuddin121@gmail.com>
2025-06-26 19:05:43 +0000

    The Netlify deploy errored, with the following guidance provided:
    ### D

--- src/app/wish/[id]/page.tsx
+++ src/app/wish/[id]/page.tsx
@@ -2,7 +2,6 @@
 
 import { useSearchParams } from 'next/navigation';
 import NightSkyTemplate from '@/app/templates/night-sky/NightSkyTemplate';
-import PremiumNightSkyTemplate from '@/app/templates/premium-night-sky/PremiumNightSkyTemplate';
 
 export default function WishPage() {
   const searchParams = useSearchParams();
@@ -17,8 +16,6 @@ export default function WishPage() {
   switch (template) {
     case 'night-sky':
       return <NightSkyTemplate {...props} />;
-    case 'premium-night-sky':
-      return <PremiumNightSkyTemplate {...props} />;
     // Add other templates here in the future
     // case 'other-template':
     //   return <OtherTemplateComponent {...props} />;
